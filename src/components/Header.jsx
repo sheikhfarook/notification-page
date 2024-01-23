58feaa240eb7010beedf365a0034a36a78445a24
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-23 09:57:19 +0530

    some change the code in the header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between font-thin">
-        <h1 className="font-[800] text-[1.5rem] ">Notification</h1>
-        <p className="text-[#5E6778] font-[500] text-[1rem]  cursor-pointer hover:text-[#0A327B]">
+      <div className="flex justify-between font-thin max-sm:mb-5 ">
+        <h1 className="font-[800] text-[1.5rem] max-sm:text-[1.25rem]">
+          Notification
+        </h1>
+        <p
+          className="text-[#5E6778] font-[500] text-[1rem] cursor-pointer hover:text-[#0A327B]
+          max-sm:py-2 max-sm:text-[0.875rem]">
           Mark all as read
         </p>
       </div>
